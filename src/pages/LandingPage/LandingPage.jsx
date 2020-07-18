18b59bb91ed42561082d0898ba18d0d6510b440f
Kathleen Stickel <kstick@Kathleens-MBP.lan>
2020-07-18 16:27:27 -0500

    add image slideshow to landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -11,6 +11,43 @@ const LandingPage = (props) => {
         <div className="LandingPage">
             <h2>Create collections of your favorite corners of the universe.</h2>
             <h3><Link to='/signup'>Sign up</Link> to get started</h3>
+            <div className="landing-images-wrapper">
+                <img 
+                    src="/assets/Earthrise-Apollo-11.jpg"
+                    alt="Earthrise Apollo 11"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Crab-Nebula-Supernova-Hubble.jpg"
+                    alt="Crab Nebula Supernova"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Saturn.jpg"
+                    alt="Saturn"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Bipolar-Star-Forming-Region-Hubble.jpg"
+                    alt="Bipolar Star-Forming Region"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Mars-Jezero-Crater.jpg"
+                    alt="Mars Jezero Crater"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Nebula-Dorado-Constellation-Hubble.jpg"
+                    alt="Nebula Dorado Constellation"
+                    className="landing-image"
+                ></img>
+                <img 
+                    src="/assets/Three-Light-Year-Tall-Pillar-of-Gas-and-Dust-Hubble.jpg"
+                    alt="Three Light Year Tall Pillar of Gas and Dust"
+                    className="landing-image"
+                ></img>
+            </div>
         </div>
     
 

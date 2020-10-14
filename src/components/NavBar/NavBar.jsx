619d8b7a1ed5407e3c3105dbba0c3f4f98d7575e
Kathleen Stickel <kstick9210@outlook.com>
2020-10-14 18:25:10 -0500

    refactor navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,36 +3,26 @@ import {Link} from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = ({ user, handleLogout }) => {
-    let nav = user ?
-    <>
+
+    return (
         <nav>
             <div className="nav-wrapper"> 
                 <h1><Link to='/'>Collect the Cosmos</Link></h1>
+                {user ?
                 <ul>
                     <li><Link to='/collections'>{user.name}'s Collections</Link></li>
                     <li><Link to='/search'>Search</Link></li>
                     <li><Link to='' onClick={handleLogout}>Log Out</Link></li>
                 </ul>
-            </div>
-        </nav>
-    </>
-    :
-    <>
-        <nav>
-            <div className="nav-wrapper">
-                <h1><Link to='/'>Collect the Cosmos</Link></h1>
+                :
                 <ul>
                     <li><Link to='/login'>Log In</Link></li>
                     <li><Link to='/signup'>Sign Up</Link></li>
                 </ul>
+                }
             </div>
         </nav>
-    </>
 
-    return (
-    <>
-        {nav}
-    </>
     )
 }
 export default NavBar;
